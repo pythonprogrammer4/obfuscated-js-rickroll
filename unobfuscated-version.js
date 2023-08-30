9293cb249eabfe799f9d87dff6c1af2a7c2c94d8
pythonprogrammer4 <135458293+pythonprogrammer4@users.noreply.github.com>
2023-08-30 09:33:24 -0700

    Update unobfuscated-version.js

--- unobfuscated-version.js
+++ unobfuscated-version.js
@@ -1,2 +1,2 @@
-var r = "htt"+"ps:"+"//"+"www"+".y"+"out"+"u"+"be.c"+"om"+"/wa"+"tc"+"h?v="+"a3"+"Z7zEc7AXQ";//prevent rickroll link from appearing in obfuscated code
-window.open(r);
+var rickroll_link = "htt"+"ps:"+"//"+"www"+".y"+"out"+"u"+"be.c"+"om"+"/wa"+"tc"+"h?v="+"a3"+"Z7zEc7AXQ";//prevent rickroll link from appearing in obfuscated code
+window.open(rickroll_link);
